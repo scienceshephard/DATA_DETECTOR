0537e207c5ee63d4e743e2ebe7a5f7927ca862f9
Opeyemi Raphael-Ogbe <scienceshephard@gmail.com>
2025-09-17 21:51:14 +0100

    Added some changes

--- main.cpp
+++ main.cpp
@@ -1,29 +1,39 @@
+/*
+    This is a typical example of a this issue showing up.
+    Two threads are created to run the same function
+    But this same function uses te same variable which is a global variable counter
+    When the thread run at the background they could clash alot of time due to but of them trying to access 
+        the same variable (memory) at the same time or even trying to run the same process (method) at the same time
+    Therefore the program will not attempts its goal. The threads will never count up to it's final value.
+    They will miss their count
+*/
+
+
+
 #include <iostream>
 #include <thread>
 
-void myThread(int arg){
-    std::cout << "Hello from this thread"<< arg <<std::endl;
+int counter = 0; // A shared global variable
+
+void increment() {
+    for (int i = 0; i < 1000000; ++i) {
+        counter++; // This is the CRITICAL SECTION: a read-modify-write operation.
+    }
 }
 
-auto myLambdaThread = [](std::string message){
-    std::cout << "Lamda thread says: " << message << std::endl;
-};
+int main() {
+    std::thread t1(increment);
+    std::thread t2(increment);
 
-int main(){
+    t1.join();
+    t2.join();
 
-    std::thread t1(myThread, 42); // Thread running myThreadFunction with argument 42
-    std::thread t2(myLambdaThread, "Greetings!"); // Thread running the lambda with a string argument
-
-            std::thread backgroundThread([](){ 
-                for (int i = 0; i < 5; i++)
-                {
-                    std::cout << "Running at the bacground" << std::endl;
-                }
-                
-             });
-    t1.join(); // Wait for t1 to finish
-        backgroundThread.detach(); // Allow backgroundThread to run independently
-    t2.join(); // Wait for t2 to finish
+    std::cout << "Counter value: " << counter << std::endl;
+    // You will almost never get 1000000 due to the data race!
+    
+    std::cout <<"Press Enter to exit..." << std::endl;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
 
     return 0;
 }
